0af7ea4525e97c5fb1d8ee4ceda70883b2109cc4
Joseph Booker <josephbooker@jhu.edu>
2018-03-08 13:39:16 -0500

    Force all XFS quota setting to happen on a single thread
    This avoids contention for updating /etc/projid and /etc/projects

--- src/main/java/org/sciserver/fileservice/manager/SciserverFileserviceManagerApplication.java
+++ src/main/java/org/sciserver/fileservice/manager/SciserverFileserviceManagerApplication.java
@@ -1,12 +1,28 @@
 package org.sciserver.fileservice.manager;
 
+import java.util.concurrent.Executor;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
 @SpringBootApplication
+@EnableAsync
 public class SciserverFileserviceManagerApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(SciserverFileserviceManagerApplication.class, args);
+	}
+
+	@Bean
+	public Executor singleThreadExecutor() {
+		ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+		executor.setCorePoolSize(1);
+		executor.setMaxPoolSize(1);
+		executor.setQueueCapacity(20);
+		executor.setThreadNamePrefix("singleThreadHelper-");
+		executor.initialize();
+		return executor;
 	}
 }
--- src/main/java/org/sciserver/fileservice/manager/xfs/XFSFileServiceModule.java
+++ src/main/java/org/sciserver/fileservice/manager/xfs/XFSFileServiceModule.java
@@ -4,6 +4,7 @@ import org.sciserver.fileservice.manager.FileServiceModule;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Profile;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -12,6 +13,7 @@ public class XFSFileServiceModule implements FileServiceModule {
 	private final Logger logger = LoggerFactory.getLogger(XFSFileServiceModule.class);
 
 	@Override
+	@Async
 	public void setQuota(String filePath, long numberOfBytes) {
 		logger.info("Setting quota to {} bytes on {}", numberOfBytes, filePath);
 	}
